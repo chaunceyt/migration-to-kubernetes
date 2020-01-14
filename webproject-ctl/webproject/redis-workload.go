392aff26aaa22ec81eaff2b273e98e195b12a215
Chauncey Thorn <chaunceyt@gmail.com>
2020-01-14 11:53:08 -0500

    NOTICKET: Updated license year

--- webproject-ctl/webproject/redis-workload.go
+++ webproject-ctl/webproject/redis-workload.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 Chauncey Thorn <chaunceyt@gmail.com>
+// Copyright © 2020 Chauncey Thorn <chaunceyt@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
